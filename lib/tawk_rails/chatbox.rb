5cd073fc357a15300becfd5f31a6ca3cbc07ebee
Sebastian Velandia <sebas.velandia@grupo.ly>
2016-01-28 13:51:31 -0500

    Fixed bug:  Tawk turbolinks compatible script, set the  variable as undefined

--- lib/tawk_rails/chatbox.rb
+++ lib/tawk_rails/chatbox.rb
@@ -4,7 +4,7 @@ module TawkRails
   class Chatbox
     def render_script
       <<-JAVASCRIPT
-        <script type="text/javascript">var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/#{TawkRails.configuration.id_site}/default';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();</script>
+        <script type="text/javascript">window.$_Tawk = undefined; var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/#{TawkRails.configuration.id_site}/default';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();</script>
       JAVASCRIPT
     end
   end
